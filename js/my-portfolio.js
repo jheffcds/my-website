88d3351c31c3e6ab9b4ec39f4be4045ca5a70f09
jheffcds <jheffcds@gmail.com>
2024-08-03 23:35:27 +0200

    Update my-portfolio.js

--- js/my-portfolio.js
+++ js/my-portfolio.js
@@ -1,4 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
 let originalText = '';
+
 function toggleSection(id) {
     const sectionContent = document.getElementById(id);
     const button = document.getElementById(id + 'Button');
@@ -14,10 +17,42 @@ function toggleSection(id) {
                 // Save the original HTML content only once
                 originalText = document.getElementById('typeEffect').innerHTML;
             }
+            typeEffect();
         }
     }
 }
 
-// Add the event listener for the hamburger menu
-const toggleButton = document.getElementById('toggle-button');
-toggleButton.addEventListener('click', toggleMenu);
+function typeEffect() {
+    const element = document.getElementById('typeEffect');
+    const text = originalText; // Use the saved original HTML content
+    element.innerHTML = ''; // Clear the element's content
+    let i = 0;
+    const speed = 15; // Speed of typing in milliseconds
+
+    function type() {
+        if (i < text.length) {
+            let char = text.charAt(i);
+            if (char === '<') {
+                // Handle HTML tags
+                let tag = '';
+                while (char !== '>') {
+                    tag += char;
+                    i++;
+                    char = text.charAt(i);
+                }
+                tag += char; // Include closing '>'
+                element.innerHTML += tag; // Insert HTML tag
+            } else {
+                element.innerHTML += char; // Insert character
+            }
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+    // Add the event listener for the hamburger menu
+    const toggleButton = document.getElementById('toggle-button');
+    toggleButton.addEventListener('click', toggleMenu);
+});
