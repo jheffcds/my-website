a5cee5889bebc88f2aded59b3e52174342f52a28
jheffcds <jheffcds@gmail.com>
2024-08-04 17:33:45 +0200

    Update my-portfolio.js

--- js/my-portfolio.js
+++ js/my-portfolio.js
@@ -2,55 +2,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 let originalText = '';
 
-function toggleSection(id) {
-    const sectionContent = document.getElementById(id);
-    const button = document.getElementById(id + 'Button');
+function toggleSection(sectionId, buttonId) {
+    const section = document.getElementById(sectionId);
+    const button = document.getElementById(buttonId);
 
-    if (sectionContent.style.display === 'block' || sectionContent.style.display === '') {
-        sectionContent.style.display = 'none';
-        button.innerHTML = '&#10095;';
+    if (section.style.display === 'none' || section.style.display === '') {
+        section.style.display = 'block';
+        button.innerHTML = '&#10094;'; // Change to "expand less" symbol
     } else {
-        sectionContent.style.display = 'block';
-        button.innerHTML = '&#10094;';
-        if (id === 'aboutMe') {
-            if (!originalText) {
-                // Save the original HTML content only once
-                originalText = document.getElementById('typeEffect').innerHTML;
-            }
-            typeEffect();
-        }
+        section.style.display = 'none';
+        button.innerHTML = '&#10095;'; // Change to "expand more" symbol
     }
 }
 
-function typeEffect() {
-    const element = document.getElementById('typeEffect');
-    const text = originalText; // Use the saved original HTML content
-    element.innerHTML = ''; // Clear the element's content
-    let i = 0;
-    const speed = 15; // Speed of typing in milliseconds
-
-    function type() {
-        if (i < text.length) {
-            let char = text.charAt(i);
-            if (char === '<') {
-                // Handle HTML tags
-                let tag = '';
-                while (char !== '>') {
-                    tag += char;
-                    i++;
-                    char = text.charAt(i);
-                }
-                tag += char; // Include closing '>'
-                element.innerHTML += tag; // Insert HTML tag
-            } else {
-                element.innerHTML += char; // Insert character
-            }
-            i++;
-            setTimeout(type, speed);
-        }
-    }
-    type();
-}
 
     // Add the event listener for the hamburger menu
     const toggleButton = document.getElementById('toggle-button');
